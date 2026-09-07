44e1704888873f686171af65e4a712e849b4df2b
zznidar <zznidar@users.noreply.github.com>
2026-09-07 21:15:51 +0200

    Make pannel responsive to scroll

--- scripts/ui.js
+++ scripts/ui.js
@@ -922,3 +922,14 @@ async function changeTimetableDate(offsetDays) {
     //menuOpen();
 
 }
+
+document.addEventListener("scrollend", menuCloseByScrollPosition);
+
+function menuCloseByScrollPosition() {
+    console.log("Scroll position: ", window.scrollX, window.scrollY);
+    if(window.scrollY <= 47) {
+        menuClose();
+    } else if(window.scrollY >= 55) {
+        document.getElementById('menu').classList.remove('closed');
+    }
+}
